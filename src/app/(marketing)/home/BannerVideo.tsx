0dc60dbc652ebeb7b9d27ced40b5b0ac57bf6044
rawatajay1998 <rawatajay9092@gmail.com>
2025-05-25 23:27:27 +0530

    Fixed and tested all final fixed 2222222

--- src/app/(marketing)/home/BannerVideo.tsx
+++ src/app/(marketing)/home/BannerVideo.tsx
@@ -11,27 +11,13 @@ const BannerVideo = React.memo(() => {
         muted
         playsInline
         preload="metadata"
+        poster="/assets/images/about-one.jpg"
       >
-        {/* Full HD for screens >= 1920px */}
-        <source
-          src="https://res.cloudinary.com/dxdgzojw6/video/upload/f_auto,q_auto:eco,vc_auto,w_1920,h_1080/v1747917983/ace-elite-banner-video_nlthyz.mp4"
-          type="video/mp4"
-          media="(min-width: 1280px)"
-        />
         {/* Mid-quality for desktop (1024px to 1919px) */}
         <source
-          src="https://res.cloudinary.com/dxdgzojw6/video/upload/f_auto,q_auto:eco,vc_auto,w_1280,h_720/v1747917983/ace-elite-banner-video_nlthyz.mp4"
+          src="https://res.cloudinary.com/dxdgzojw6/video/upload/f_auto,q_auto:eco,vc_auto/v1747917983/ace-elite-banner-video_nlthyz.mp4"
           type="video/mp4"
-          media="(min-width: 768px) and (max-width: 1100px)"
         />
-        {/* Lower-quality for mobile (<768px) */}
-        <source
-          src="https://res.cloudinary.com/dxdgzojw6/video/upload/f_auto,q_auto:eco,vc_auto,w_640,h_360/v1747917983/ace-elite-banner-video_nlthyz.mp4"
-          type="video/mp4"
-          media="(max-width: 767px)"
-        />
-        {/* Fallback for unsupported browsers */}
-        Your browser does not support the video tag.
       </video>
     </div>
   );
