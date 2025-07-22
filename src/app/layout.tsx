967bb0278a3b52a4f3d4f90a9b5b70d8dad4d6f5
rawatajay1998 <rawatajay9092@gmail.com>
2025-07-22 11:21:52 +0530

    Fixed and tested all final fixed 2222222222222222222222222222

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,10 +7,11 @@ import RouteLoader from "@/components/common/progressBar";
 import ScrollToTop from "@/components/common/ScrollToTop";
 import { Suspense } from "react";
 import AlwaysViewTop from "@/components/marketing/AlwaysViewTop";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   icons: {
-    icon: "/favicon.png", // Globally used favicon
+    icon: "/favicon.png",
   },
 };
 
@@ -22,12 +23,59 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Custom Font */}
         <link
           href="https://fonts.cdnfonts.com/css/futura-pt"
           rel="stylesheet"
         />
+
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-B03JVGPLE9"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-B03JVGPLE9');
+            `,
+          }}
+        />
+
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1631820150778366');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
       <body>
+        {/* Meta Pixel NoScript fallback */}
+        <noscript>
+          <Image
+            alt="Facebook Pixel Code"
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1631820150778366&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* App Components */}
         <AlwaysViewTop />
         <Suspense fallback={null}>
           <RouteLoader />
